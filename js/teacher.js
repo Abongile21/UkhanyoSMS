1cd94277b68d68a97e4610136c07186d27b6eaf8
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-10 12:08:40 +0200

    add students to gradebook

--- js/teacher.js
+++ js/teacher.js
@@ -1,3 +1,18 @@
+const tableBody = document.getElementById('tableBody');
+
+
+
+function buildRow(user){
+    return `<tr>
+                <td class="px-6 py-4 whitespace-nowrap">${user.name}</td>
+                <td class="px-6 py-4 whitespace-nowrap">Matric Second Chance</td>
+                <td class="px-6 py-4 whitespace-nowrap"><span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span></td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900" value=${user._id}>Edit</a>
+                    <a href="#" class="text-red-600 hover:text-red-900 ml-4" value=${user._id}>Delete</a>
+                </td>
+            </tr>`
+}
 
 document.getElementById('gradebook').addEventListener('click', function(){
     
@@ -12,7 +27,13 @@ document.getElementById('gradebook').addEventListener('click', function(){
         return response.json()
     })
     .then(res => {
-        console.log(res);
+        for (const element of res.data) {
+            const row = buildRow(element);
+            tableBody.insertAdjacentHTML('beforeend', row);
+        }
+    })
+    .catch(error => {
+        console.log(error);
     })
     
 });
