fc82b5ba721119e590bb3e486561fa7745e4fac9
Abongile21 <abongiletheo00@gmail.com>
2024-09-10 14:36:31 +0200

    add some  modal for subjects and marks

--- js/teacher.js
+++ js/teacher.js
@@ -6,6 +6,11 @@ function buildRow(user){
     return `<tr>
                 <td class="px-6 py-4 whitespace-nowrap">${user.name}</td>
                 <td class="px-6 py-4 whitespace-nowrap">Matric Second Chance</td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                <a href="#" class="text-blue-600 hover:text-indigo-900" onclick="showMarkModal()">View</a>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">${user.name}</td>
+
                 <td class="px-6 py-4 whitespace-nowrap"><span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span></td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                     <a href="#" class="text-indigo-600 hover:text-indigo-900" value=${user._id}>Edit</a>
@@ -13,6 +18,46 @@ function buildRow(user){
                 </td>
             </tr>`
 }
+
+
+const subjects = ['Math', 'English', 'History', 'Science', 'Art'];
+const marks = [85, 90, 78, 88, 92];
+
+
+const modal = document.getElementById('chartModal');
+const marksTableBody = document.getElementById('marksTableBody');
+
+function showMarkModal() {
+    
+
+    marksTableBody.innerHTML = '';
+
+    subjects.forEach((subject, index) => {
+        const row = document.createElement('tr');
+
+        const subjectCell = document.createElement('td');
+        subjectCell.classList.add('px-4', 'py-2', 'border');
+        subjectCell.textContent = subject;
+
+        const marksCell = document.createElement('td').appendChild(document.createElement('input'));
+
+        marksCell.classList.add('px-4', 'py-2', 'border');
+        marksCell.value = marks[index];
+
+        row.appendChild(subjectCell);
+        row.appendChild(marksCell);
+        marksTableBody.appendChild(row);
+    });
+
+    modal.classList.remove('hidden');
+};
+
+function closeModal() {
+    modal.classList.add('hidden');
+}
+
+
+
 
 document.getElementById('gradebook').addEventListener('click', function(){
     
